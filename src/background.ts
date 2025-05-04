9f40ea3acb697ec27063c6ebbb6f89fe7a2e27e1
Seth Barklow <thedudewhorocks@yahoo.com>
2025-05-04 02:41:40 -0700

    removed template, added inital cargo grid class

--- src/background.ts
+++ src/background.ts
@@ -1,15 +1,44 @@
-let active = false;
+import { Vector3 } from "./utils/Types";
 
-function makeOrange(color: string): void {
-    document.body.style.backgroundColor = color;
+class CargoGrid
+{
+    dimentions: Vector3;
+    grid: Array<Array<Array<number>>>;
+
+    constructor(length: number, width: number, height: number)
+    {
+        // Verify none of the values = 0, smallest grid can only by 1x1x1
+        if(!length&&!width&&!height) throw "Inputs cannot be 0.";
+
+        this.dimentions = {x: length, y: width, z: height};
+        this.grid = new Array();
+
+        // Loop through dimentions and map out matrix
+        for(let x=0; x<this.dimentions.x; x++)
+        {
+            this.grid[x] = new Array();
+            for(let y=0; y<this.dimentions.y; y++)
+            {
+                this.grid[x][y] = new Array();
+                for(let z=0; z<this.dimentions.z; z++)
+                {
+                    this.grid[x][y][z] = 0; // Assign 0 to indicate location is empty
+                }
+            }
+        }
+    }
+
+    get area()
+    {
+        return this.calcArea();
+    }
+
+    calcArea()
+    {
+        return this.dimentions.x * this.dimentions.y * this.dimentions.z;
+    }
 }
 
-chrome.action.onClicked.addListener((tab) => {
-    active = !active;
-    const color = active ? 'orange' : 'white';
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id ? tab.id : -1},
-        func: makeOrange,
-        args: [color]
-    }).then();
-});
+
+const newGrid = new CargoGrid(10, 2, 3);
+console.log(newGrid.grid);
